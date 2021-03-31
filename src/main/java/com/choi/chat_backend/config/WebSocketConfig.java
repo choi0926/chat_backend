4eeeeeea44e3b50583dde46d971647da5c24595e
choi0926 <ghtjd753753@gmail.com>
2021-03-31 23:30:03 +0900

    AbstractMessageChannel

--- src/main/java/com/choi/chat_backend/config/WebSocketConfig.java
+++ src/main/java/com/choi/chat_backend/config/WebSocketConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -28,6 +29,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration){
-        registration.interceptors(webSocketInterceptor);
+        registration.interceptors((ChannelInterceptor) webSocketInterceptor);
     }
 }
